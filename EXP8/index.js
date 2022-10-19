3745f4e0ed9c56518cc7a2012f0700e6e4d3a420
Ayush Singh <ayush@AERO13.localdomain>
2022-10-19 21:38:01 +0530

    EXP 8 7 12

--- EXP8/index.js
+++ EXP8/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-// import Newclass from './newclass';
+
 import reportWebVitals from './reportWebVitals';
 
+//Using React
+// const myElement = <h1>This is an Element rendered using JSX in React.</h1>;
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <Newclass />
-//   </React.StrictMode>
-// );
+// root.render(myElement);
 
-const myElement = <h1>This is an Element rendered using JSX in React.</h1>;
+//Using Normal Javascript
+// ReactDOM.render(<h1>This is an Element rendered using JSX in React.</h1>,
+// document.getElementById('root'));
+ 
+//Using Babel
+ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "This is an Element rendered using JSX in React."), document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
